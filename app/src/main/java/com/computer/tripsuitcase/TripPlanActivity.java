c097d4a2e32e71b89580eef552e8848d62c2d68f
Yuksel Cosgun <yukselcosgun@hotmail.com.tr>
2024-05-30 19:59:27 +0300

    firebase first implement and activityTripPlan create

--- app/src/main/java/com/computer/tripsuitcase/TripPlanActivity.java
+++ app/src/main/java/com/computer/tripsuitcase/TripPlanActivity.java
@@ -2,13 +2,37 @@ package com.computer.tripsuitcase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class TripPlanActivity extends AppCompatActivity {
 
+    CalendarView calendarView;
+    Button btnTrip;
+    TextView textTrip;
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_plan);
+
+        calendarView=findViewById(R.id.calendarView);
+        btnTrip = findViewById(R.id.btnTripPlan);
+        textTrip = findViewById(R.id.textTrip);
+
+        btnTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendarView.getDateTextAppearance();
+
+            }
+        });
+
+
     }
 }
